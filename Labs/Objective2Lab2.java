7c145dc198387da5f8f9a5c2a2bd6847bbb3a159
lightningclear <voh777@yahoo.com>
2022-01-22 00:19:28 -0700

    completed Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Henry Vo";
+    int age = 24;
+    String state = "Colorado";
+      System.out.println(name);
+      System.out.println(age);
+      System.out.println(state);
+  }
+}
