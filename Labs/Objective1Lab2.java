599152b29688407e5edcaedbd4b8e37dc3575360
lightningclear <voh777@yahoo.com>
2022-01-21 23:57:30 -0700

    complete objective1Lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,11 @@
+public class Objective1Lab2 {
+  public static void main(String[] args) {
+    System.out.println("Grocery List");
+    System.out.println("----------");
+    System.out.println("1. Potatos");
+    System.out.println("2. Butter");
+    System.out.println("3. Onions");
+    System.out.println("4. Bacon");
+    System.out.println("----------");
+  }
+}
