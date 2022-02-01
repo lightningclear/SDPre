f2010568ec1fe2262853f79a6d540648512a7a79
lightningclear <voh777@yahoo.com>
2022-02-01 16:18:51 -0700

    complete objective6lab1

---
+++ Labs/Objective6Lab1.java
@@ -0,0 +1,14 @@
+public class Objective6Lab1 {
+  public static void main(String[] args) {
+    int month = 6;
+
+    switch (month) {
+      case 1:
+        System.out.println("Janurary");
+        break;
+      case 6:
+        System.out.println("June");
+        break;
+    }
+  }
+}
