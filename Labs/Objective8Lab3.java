a42c332055a787efd058f2fc551a43e8d8925912
lightningclear <voh777@yahoo.com>
2022-02-04 16:41:11 -0700

    no u

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,13 @@
+public class Objective8Lab3 {
+  public static void main(String[]args) {
+    int counter = 1;
+    for(int i = 1; i <= 20; i += 1){
+      if(i % 2 == 0){
+        System.out.println(i + "is even");
+      }else{
+        System.out.println(i + "is Odd");
+      }
+    }
+    counter = counter + 1;
+  }
+}
