62affcbc1f2573ae6718e5bb6ffa9ec70b12ac4d
lightningclear <voh777@yahoo.com>
2022-02-04 16:18:35 -0700

    objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[]args) {
+    for (int i = 0; i < 11; i += 1){
+    System.out.println(i);
+    }
+    for (int v = 10; v > 0; v -= 1){
+    System.out.println(v);
+    }
+  }
+}
