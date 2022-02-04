80d9e89ad6332d6aeb34f9c575833355aaf1c167
lightningclear <voh777@yahoo.com>
2022-02-04 15:31:50 -0700

    completed obj7L4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4{
+  public static void main(String[] args){
+    int counter = 0;
+    int sum = 0;
+
+    while (counter <= 19){
+      counter = counter + 1;
+
+      sum = counter + sum;
+    }
+    System.out.println(sum);
+  }
+}
